012508be3397554f10b35f2eb28601504e843a3c
rahulp <rahulp5875@gmail.com>
2025-03-13 16:05:00 +0530

    Egg counter completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let hardness: [String: Int] = [
+        "Soft": 3,
+        "Medium": 4,
+        "Hard": 7
+    ]
+    
+    var timer: Timer? = nil
+    var secondsPassed: Int = 0
+    var totalTime: Int = 0
 
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBAction func onHardnessSelected(_ sender: UIButton) {
+        
+        print(sender.currentTitle!)
+        if sender.currentTitle != nil {
+            startTimer(value: hardness[sender.currentTitle!]!)
+        }
+        
+    }
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    func startTimer(value: Int){
+        progressView.progress = 0.0
+        secondsPassed = 0
+        totalTime = value
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter(){
+        if secondsPassed < totalTime{
+            print(secondsPassed)
+            progressView.progress = Float(secondsPassed)/Float(totalTime)
+            secondsPassed += 1
+            
+        } else {
+            timer?.invalidate()
+            titleLabel.text = "Done!"
+            progressView.progress = 1.0
+        }
+    }
+    
 }
